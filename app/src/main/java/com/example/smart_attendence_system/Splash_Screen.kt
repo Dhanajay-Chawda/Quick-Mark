bd78c00c774db347d0dacc2dfc98c5c2c2824962
Dhananjay-Chawda <dhananjaychawda23@gmail.com>
2023-03-23 17:55:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/smart_attendence_system/Splash_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.smart_attendence_system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
